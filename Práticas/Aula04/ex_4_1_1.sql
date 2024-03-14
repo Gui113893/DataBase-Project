49d133fcd056988e56bb82a8e8f8b506ccd5062f
Guilherme Santos <119808297+Gui113893@users.noreply.github.com>
2024-03-14 17:38:07 +0000

    commit

--- Práticas/Aula04/ex_4_1_1.sql
+++ Práticas/Aula04/ex_4_1_1.sql
@@ -8,22 +8,13 @@ IF NOT EXISTS (
 )
 CREATE DATABASE DatabaseName
 
--- Create the schema if it does not exist already
-USE Ex4_1_1
-GO
-IF NOT EXISTS (
-    SELECT schema_name
-        FROM information_schema.schemata
-        WHERE schema_name = N'RentACar'
-)
-EXEC('CREATE SCHEMA RentACar')
 
--- Create a new table called 'CLIENTE' in schema 'RentACar'
+-- Create a new table called 'CLIENTE''
 -- Drop the table if it already exists
-IF OBJECT_ID('RentACar.CLIENTE', 'U') IS NOT NULL
-DROP TABLE RentACar.CLIENTE
+IF OBJECT_ID('CLIENTE', 'U') IS NOT NULL
+DROP TABLE CLIENTE
 GO
-CREATE TABLE RentACar.CLIENTE
+CREATE TABLE CLIENTE
 (
     NIF         INT             PRIMARY KEY     NOT NULL    CHECK (NIF >= 100000000 AND NIF <= 999999999),
     nome        VARCHAR(100)                    NOT NULL,
@@ -31,13 +22,13 @@ CREATE TABLE RentACar.CLIENTE
     num_carta   INT             UNIQUE          NOT NULL    CHECK (num_carta >= 100000000 AND num_carta <= 999999999),
 );
 
--- Create a new table called 'ALUGUER' in schema 'RentACar'
+-- Create a new table called 'ALUGUER'
 -- Drop the table if it already exists
-IF OBJECT_ID('RentACar.ALUGUER', 'U') IS NOT NULL
-DROP TABLE RentACar.ALUGUER
+IF OBJECT_ID('ALUGUER', 'U') IS NOT NULL
+DROP TABLE ALUGUER
 GO
 
-CREATE TABLE RentACar.ALUGUER
+CREATE TABLE ALUGUER
 (
     numero      INT             PRIMARY KEY     NOT NULL,
     data        DATE                            NOT NULL,
@@ -46,18 +37,18 @@ CREATE TABLE RentACar.ALUGUER
     balcao      INT                             NOT NULL,
     veiculo     INT                             NOT NULL,
 
-    FOREIGN KEY (cliente) REFERENCES RentACar.CLIENTE(NIF),
-    FOREIGN KEY (balcao) REFERENCES RentACar.BALCAO(numero),
+    FOREIGN KEY (cliente) REFERENCES CLIENTE(NIF),
+    FOREIGN KEY (balcao) REFERENCES BALCAO(numero),
 );
 GO
 
--- Create a new table called 'BALCAO' in schema 'RentACar'
+-- Create a new table called 'BALCAO' in schema '
 -- Drop the table if it already exists
-IF OBJECT_ID('RentACar.BALCAO', 'U') IS NOT NULL
-DROP TABLE RentACar.BALCAO
+IF OBJECT_ID('BALCAO', 'U') IS NOT NULL
+DROP TABLE BALCAO
 GO
 -- Create the table in the specified schema
-CREATE TABLE RentACar.BALCAO
+CREATE TABLE BALCAO
 (
     numero      INT             PRIMARY KEY     NOT NULL,
     nome        VARCHAR(100),                    
@@ -65,83 +56,83 @@ CREATE TABLE RentACar.BALCAO
 );
 GO
 
--- Create a new table called 'VEICULO' in schema 'RentACar'
+-- Create a new table called 'VEICULO' in schema '
 -- Drop the table if it already exists
-IF OBJECT_ID('RentACar.VEICULO', 'U') IS NOT NULL
-DROP TABLE RentACar.VEICULO
+IF OBJECT_ID('VEICULO', 'U') IS NOT NULL
+DROP TABLE VEICULO
 GO
 -- Create the table in the specified schema
-CREATE TABLE RentACar.VEICULO
+CREATE TABLE VEICULO
 (
     matricula       CHAR(8)         PRIMARY KEY     NOT NULL,
     marca           VARCHAR(100)                    NOT NULL,
     tipo_veiculo    VARCHAR(100)                    NOT NULL,
     ano             INT                             NOT NULL,
 
-    FOREIGN KEY (tipo_veiculo) REFERENCES RentACar.TIPO_VEICULO(codigo),
+    FOREIGN KEY (tipo_veiculo) REFERENCES TIPO_VEICULO(codigo),
 );
 GO
 
--- Create a new table called 'TIPO_VEICULO' in schema 'RentACar'
+-- Create a new table called 'TIPO_VEICULO' in schema '
 -- Drop the table if it already exists
-IF OBJECT_ID('RentACar.TIPO_VEICULO', 'U') IS NOT NULL
-DROP TABLE RentACar.TIPO_VEICULO
+IF OBJECT_ID('TIPO_VEICULO', 'U') IS NOT NULL
+DROP TABLE TIPO_VEICULO
 
 -- Create the table in the specified schema
-CREATE TABLE RentACar.TIPO_VEICULO
+CREATE TABLE TIPO_VEICULO
 (
     codigo          INT             PRIMARY KEY     NOT NULL,
     arcondicionado  BIT,
     designacao      VARCHAR(100)                    NOT NULL, 
 );
 
--- Create a new table called 'SIMILARIDADE' in schema 'RentACar'
+-- Create a new table called 'SIMILARIDADE' in schema '
 -- Drop the table if it already exists
-IF OBJECT_ID('RentACar.SIMILARIDADE', 'U') IS NOT NULL
-DROP TABLE RentACar.SIMILARIDADE
+IF OBJECT_ID('SIMILARIDADE', 'U') IS NOT NULL
+DROP TABLE SIMILARIDADE
 GO
 -- Create the table in the specified schema
-CREATE TABLE RentACar.SIMILARIDADE
+CREATE TABLE SIMILARIDADE
 (
     cod_veiculo1    INT                NOT NULL,
     cod_veiculo2    INT                NOT NULL,
 
-    FOREIGN KEY (cod_veiculo1) REFERENCES RentACar.TIPO_VEICULO(codigo),
-    FOREIGN KEY (cod_veiculo2) REFERENCES RentACar.TIPO_VEICULO(codigo),
+    FOREIGN KEY (cod_veiculo1) REFERENCES TIPO_VEICULO(codigo),
+    FOREIGN KEY (cod_veiculo2) REFERENCES TIPO_VEICULO(codigo),
     PRIMARY KEY (cod_veiculo1, cod_veiculo2),
 );
 GO
 
--- Create a new table called 'LIGEIRO' in schema 'RentACar'
+-- Create a new table called 'LIGEIRO' in schema '
 -- Drop the table if it already exists
-IF OBJECT_ID('RentACar.LIGEIRO', 'U') IS NOT NULL
-DROP TABLE RentACar.LIGEIRO
+IF OBJECT_ID('LIGEIRO', 'U') IS NOT NULL
+DROP TABLE LIGEIRO
 GO
 -- Create the table in the specified schema
-CREATE TABLE RentACar.LIGEIRO
+CREATE TABLE LIGEIRO
 (
     codigo         INT             PRIMARY KEY      NOT NULL,
     numlugares     INT                              NOT NULL,
     portas         INT                              NOT NULL,
     combustivel    VARCHAR(100)                     NOT NULL,
 
-    FOREIGN KEY (codigo) REFERENCES RentACar.TIPO_VEICULO(codigo),
+    FOREIGN KEY (codigo) REFERENCES TIPO_VEICULO(codigo),
 );
 GO
 
 
--- Create a new table called 'PESADO' in schema 'RentACar'
+-- Create a new table called 'PESADO' in schema '
 -- Drop the table if it already exists
-IF OBJECT_ID('RentACar.PESADO', 'U') IS NOT NULL
-DROP TABLE RentACar.PESADO
+IF OBJECT_ID('PESADO', 'U') IS NOT NULL
+DROP TABLE PESADO
 GO
 -- Create the table in the specified schema
-CREATE TABLE RentACar.PESADO
+CREATE TABLE PESADO
 (
     codigo         INT             PRIMARY KEY      NOT NULL,
     peso           INT                              NOT NULL,
     passageiros    INT                              NOT NULL,
 
-    FOREIGN KEY (codigo) REFERENCES RentACar.TIPO_VEICULO(codigo),
+    FOREIGN KEY (codigo) REFERENCES TIPO_VEICULO(codigo),
 );
 GO
